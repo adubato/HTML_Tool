7891981aafe39c242416a92e7c6c76b8045c7add
adubato <adubato.michael41@student.ccm.edu>
2016-05-02 16:47:23 -0400

    htmlReader works

--- htmlReader.java
+++ htmlReader.java
@@ -1,38 +1,50 @@
 import java.io.*;
-import java.util.Scanner;
 
 public class htmlReader {
 	
-	private File htmlFile;
-	public String [] lines;
-	Scanner scanner;
+	private BufferedReader reader_;
+	public String [] lines_;
+	private int numLines_ = -1;
 	
-	public void readHtml(String filename) throws IOException{	
-			htmlFile = new File(filename + ".htm");
-			scanner = new Scanner(htmlFile);
-			lines = new String[];
-					
-			String strLine;
-			
-			int i = 0;
-			//Read File Line By Line
-			while(scanner.hasNextLine()) {
-				lines[i++] = scanner.nextLine();
-				//System.out.println(scanner.nextLine());
-			}
-			
-			scanner.close();
+	public htmlReader(String filename) throws IOException {	
+		reader_ = new BufferedReader(new FileReader(filename + ".htm")); // open file
+
+		numLines_ = 0; // initialize numLines_ to 0
+		while (reader_.readLine() != null) numLines_++; // count lines
+		
+		lines_ = new String[numLines_]; // allocate lines_ array using numLines_
+		
+		reader_ = new BufferedReader(new FileReader(filename + ".htm")); // re-open file to reset reader_
+				
+		for(int i = 0; i < lines_.length; i++) lines_[i] = reader_.readLine(); // assign each line to position in array using reader_
+		
+		reader_.close();
+	}
+	
+	public String[] getLines() {
+		return lines_;
+	}
+	
+	public String getLineAtIndex(int index) {
+		try { return lines_[index]; }
+		catch(ArrayIndexOutOfBoundsException e) { return e.toString(); }
+	}
+	
+	public int getNumLines() {
+		return numLines_;
 	}
 
 	public static void main(String[] args) throws IOException {
 		
-		htmlReader r = new htmlReader();
+		htmlReader r = new htmlReader("ExampleDomain");
+		String [] lines = r.getLines();
 		
-		r.readHtml("ExampleDomain");
-		
-		for(String s : r.lines) {
+		for(String s : lines)
 			System.out.println(s);
-		}
+		
+		System.out.println(r.getNumLines());
+		System.out.println(r.getLineAtIndex(50));
+
 	}
 
 }
