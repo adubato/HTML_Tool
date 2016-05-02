22e04a7db701b8a91535bec92b6eee5ef37a2dbd
adubato <adubato.michael41@student.ccm.edu>
2016-05-02 17:23:25 -0400

    htmlReader works

--- htmlReader.java
+++ htmlReader.java
@@ -1,27 +1,42 @@
+/** A class used to read html files elements in Java. 
+ ** @author Michael Adubato **/
 import java.io.*;
+import java.util.*;
 
 public class htmlReader {
 	
 	private BufferedReader reader_;
-	public String [] lines_;
+	private String [] lines_;
 	private int numLines_ = -1;
 	
+	private List<element> elements_;
+	
 	public htmlReader(String filename) throws IOException {	
-		reader_ = new BufferedReader(new FileReader(filename + ".htm")); // open file
+		try { reader_ = new BufferedReader(new FileReader(filename + ".htm")); } // open file
+		catch(FileNotFoundException e) { reader_ = new BufferedReader(new FileReader(filename + ".html")); }
 
 		numLines_ = 0; // initialize numLines_ to 0
 		while (reader_.readLine() != null) numLines_++; // count lines
 		
 		lines_ = new String[numLines_]; // allocate lines_ array using numLines_
 		
-		reader_ = new BufferedReader(new FileReader(filename + ".htm")); // re-open file to reset reader_
-				
+		try { reader_ = new BufferedReader(new FileReader(filename + ".htm")); } // re-open file to reset reader_
+		catch(FileNotFoundException e) { reader_ = new BufferedReader(new FileReader(filename + ".html")); }
+		
 		for(int i = 0; i < lines_.length; i++) lines_[i] = reader_.readLine(); // assign each line to position in array using reader_
 		
 		reader_.close();
+		
+		parseElements(lines_);
 	}
 	
-	public String[] getLines() {
+	private void parseElements(String [] lines) {		
+		for(String line : lines) {
+
+		}
+	}
+	
+	public String [] getLines() {
 		return lines_;
 	}
 	
@@ -44,6 +59,15 @@ public class htmlReader {
 		
 		System.out.println(r.getNumLines());
 		System.out.println(r.getLineAtIndex(50));
+		
+		htmlReader r2 = new htmlReader("ExampleDomain_COPY");
+		lines = r.getLines();
+		
+		for(String s : lines)
+			System.out.println(s);
+		
+		System.out.println("r2 " + r2.getNumLines());
+		System.out.println("r2 " + r2.getLineAtIndex(12));
 
 	}
 
